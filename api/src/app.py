861d8ae83613b6351544872c6e2c6d4ec2ca3067
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-02-28 21:50:48 -0500

    Fix favicon route

--- api/src/app.py
+++ api/src/app.py
@@ -1,3 +1,4 @@
+from starlette.responses import FileResponse
 
 from src.config import app
 from src.routers import (auth)
@@ -7,3 +8,8 @@ app.include_router(auth.router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to Snapping Rails!"}
+
+
+@app.get('/favicon.ico')
+async def favicon():
+    return FileResponse('favicon.ico')
