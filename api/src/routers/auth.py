76dee3d3d33e11387914a0ebbfca8225f46a54e3
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-03-01 07:49:32 -0500

    Set up skeleton for markers routerr

--- api/src/routers/auth.py
+++ api/src/routers/auth.py
@@ -116,7 +116,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     return user
 
 
-
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
---
+++ api/src/routers/markers.py
@@ -0,0 +1,52 @@
+from fastapi import Depends, HTTPException, status, APIRouter
+from pydantic import BaseModel, validator, PastDate, HttpUrl
+from typing import List
+
+from src.config import oauth2_scheme
+from src.database.database import SNAPPING_RAILS_ENGINE as db
+from src.database import models
+from src.database.functions import SqlalchemyResult
+
+
+router = APIRouter()
+
+
+class Marker(BaseModel):
+    created_at: PastDate
+    lat: float
+    long: float
+    media_url: HttpUrl
+    title: str
+    description: str
+
+    @validator('lat')
+    def validate_lat_bounds(cls, v):
+        if v < -90 or v > 90:
+            raise ValueError("Latitude value isn't valid")
+        return v
+
+    @validator('long')
+    def validate_long_bounds(cls, v):
+        if v < -180 or v > 180:
+            raise ValueError("Longitude value isn't valid")
+        return v
+
+
+@router.get("/markers")
+async def get_railmap_markers():
+    pass
+
+
+@router.post("/markers", status_code=status.HTTP_201_CREATED)
+async def add_railmap_markers(markers: List[Marker]):
+    pass
+
+
+@router.put("/markers")
+async def update_railmap_markers(markers: List[Marker]):
+    pass
+
+
+@router.delete("/markers")
+async def register_new_user(markers: List[Marker]):
+    pass
