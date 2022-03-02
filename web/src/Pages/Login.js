86a2034876ea098a5912c972bc2cd0aac5618040
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-03-02 09:46:45 -0500

    I cleaned Login more

--- web/src/Pages/Login.js
+++ web/src/Pages/Login.js
@@ -2,36 +2,31 @@
 import Email from '@mui/icons-material/Email'
 import Lock from '@mui/icons-material/Lock';
 
-import Grid from '@mui/material/Grid';
-import CssBaseline from '@mui/material/CssBaseline';
-import InputAdornment from '@mui/material/InputAdornment'
-import Typography from '@mui/material/Typography';
+import {Grid, CssBaseline, InputAdornment, Typography, Container} from '@mui/material';
 import LoginForm from '../Components/Forms/LoginForm'
 
 
 const Login = () => {
-
-
     return (
         <div>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Grid
-                        container
-                        spacing={0}
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                        style={{ minHeight: '90vh' }}
-                    >
-                    <Typography component="h1" variant="h3">
-                        All Aboard!
-                    </Typography>
+          <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Grid
+                  container
+                  spacing={0}
+                  direction="column"
+                  alignItems="center"
+                  justifyContent="center"
+                  style={{ minHeight: '90vh' }}
+              >
+              <Typography component="h1" variant="h3">
+                  All Aboard!
+              </Typography>
 
-                    <LoginForm />
-  
-                    </Grid>
-                </Container>
+              <LoginForm />
+
+              </Grid>
+          </Container>
         </div>
     );
 }
