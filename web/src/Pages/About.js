6e6990983d625525c587c8a740984e72b61eebc9
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-03-02 19:31:45 -0500

    Added some missing pages

--- web/src/Pages/About.js
+++ web/src/Pages/About.js
@@ -1,10 +1,26 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 const About = () => {
 
     return (
-        <Container className="Content-body">
-            <h1>About</h1>
+        <Container className="Content-body" maxWidth="sm">
+            <Typography variant="h2">About Snapping Rails</Typography>
+            <article>
+                Snapping Rails exists to provide a platform for those who love the history of the world's many railroads, and who are excited for their futures.
+                <br/><br/>
+                In the United States, the railroads catalyzed the expansion of our nation and the placements and layouts of our towns and cities are largely
+                a result of their presence. Learning about the railroads is a way to learn about our own histories, and to build a sense of place in our hometowns.
+                <br/><br/>
+                As time moves forward I believe rail-based transportation and logistics will become ever more critical, and I cannot wait to see railways
+                be expanded and popularized-- especially for passenger traffic once more. 
+                <br/><br/>
+                I want this platform to serve the railfanning community in cataloging
+                their exploits out on the high iron as they snap coveted shops of various locomotives and equipment. This will be an attempt to drive
+                enthusiasm for what I personally deem to be a most worthwhile hobby, and to act as a place to connect with others that share our passion
+                for trains.
+                <br/><br/>
+                I am honored to have you here as a guest, and I hope that you enjoy Snapping Rails!
+            </article>
        </Container>
     );
 }
--- web/src/Pages/Contact.js
+++ web/src/Pages/Contact.js
@@ -1,10 +1,14 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 const Contact = () => {
 
     return (
         <Container className="Content-body">
-            <h1>Contact</h1>
+            <Typography variant="h1">Contact</Typography>
+
+            <h2><a href="https://github.com/ThorntonMatthewD/snapping-rails/issues">Click here to submit an issue</a></h2>
+
+
         </Container>
     );
 }
