f507ebf5e14a62f9072716a1852c293d13210bad
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-03-03 14:12:05 -0500

    Write first test for api

---
+++ api/tests/test_main.py
@@ -0,0 +1,22 @@
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import pytest
+from src.app import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_root_route():
+    response = client.get("/")
+    
+    assert response.status_code == 200, f"Root route could need be reached. HTTP {response.status_code}"
+    assert response.json() == {'message': 'Welcome to Snapping Rails!'}
+
+
+if __name__ == "__main__":
+  test_root_route()
