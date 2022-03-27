110badc26ab37de817615edc3064f02af8db0916
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-03-27 11:22:18 -0400

    Update TEST_USER

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,5 +1,5 @@
 export const STRINGS = {
   TEST_TOKEN:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NjcxMjM0MzIxIiwibmFtZSI6Ikp1c3QgVGVzdGluISIsImlhdCI6MTUxNjIzOTAyMn0.hWZq8sf3JlYyiL8wASryClQZT5VNiln8_a2jIR9GwZs",
-  TEST_USER: { sub: "Bob" },
+  TEST_USER: { username: "Bob" },
 };
