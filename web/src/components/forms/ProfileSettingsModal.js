30dfd40278b380d9a26cc76dc3977cefcfa9739d
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-04-24 23:08:35 -0400

    Create basic test.. Tiime to go to bed.

---
+++ web/src/components/forms/ProfileSettingsModal.js
@@ -0,0 +1,3 @@
+const ProfileSettingsModal = () => {};
+
+export default ProfileSettingsModal;
---
+++ web/src/tests/components/forms/ProfileSettingsModal.test.js
@@ -0,0 +1,16 @@
+import { cleanup, render, screen } from "@testing-library/react";
+
+import ProfileSettingsModal from "../../components/forms/ProfileSettingsModal";
+
+afterEach(() => {
+  cleanup();
+});
+
+const renderProfileSettingsModal = () => {
+  return render(<ProfileSettingsModal />);
+};
+
+it("shows information passed in from server", () => {
+  renderProfileSettingsModal();
+  expect(true).toBe(true);
+});
