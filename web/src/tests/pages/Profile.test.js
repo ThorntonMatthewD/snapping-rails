ff06527c61afa49dfa2c371a835e45f657e9bca2
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-04-24 22:03:52 -0400

    Add some more frontend tests for the profile page

--- web/src/tests/pages/Profile.test.js
+++ web/src/tests/pages/Profile.test.js
@@ -1,19 +1,17 @@
-import { cleanup, render, screen, wait } from "@testing-library/react";
-import { createMemoryHistory } from "history";
-import { within } from "@testing-library/dom";
-import { MemoryRouter } from "react-router-dom";
+import { cleanup, render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 import { AuthProvider } from "../../contexts/AuthProvider";
 import Profile from "../../pages/Profile";
 import { STRINGS } from "../../constants";
 
 const renderProfile = (loggedInUser, initialURL = "/profile") => {
-  const history = createMemoryHistory({ initialEntries: [initialURL] });
-
   return render(
-    <MemoryRouter history={history}>
+    <MemoryRouter initialEntries={[initialURL]}>
       <AuthProvider initialUser={loggedInUser}>
-        <Profile />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </AuthProvider>
     </MemoryRouter>
   );
@@ -39,6 +37,49 @@ describe("Whenever loading a profile page", () => {
 
     const postsTitle = await screen.findByText(/bob's posts/i);
 
+    expect(fetch).toHaveBeenCalledTimes(2);
     expect(postsTitle).toBeTruthy();
+  });
+
+  it("the page shows the information for a user given a username URL query parameter", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        username: "George",
+        profile_description: "Hi, I'm not Bob.",
+      }),
+      { status: 200 }
+    );
+
+    renderProfile(null, "/profile?username=George");
+
+    const postsTitle = await screen.findByText(/george's posts/i);
+
+    expect(fetch).toHaveBeenCalledTimes(2);
+    expect(postsTitle).toBeTruthy();
+  });
+
+  it("the page stays on the loading screen if no user is logged in and no valid username is provided", async () => {
+    renderProfile(null);
+
+    const loadingTitle = await screen.findByText(/Grabbing user info../i);
+
+    expect(fetch).toHaveBeenCalledTimes(0);
+    expect(loadingTitle).toBeTruthy();
+  });
+
+  it("the page shows an error message if the server gives an error response", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        detail: "Bob is no longer with us.. ",
+      }),
+      { status: 404 }
+    );
+
+    renderProfile(null, "/profile?username=Bob");
+
+    const errorTitle = await screen.findByText(/An Error Has Occurred/i);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(errorTitle).toBeTruthy();
   });
 });
