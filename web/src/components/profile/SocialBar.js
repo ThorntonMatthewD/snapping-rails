3ed47830d68bc6abe34f30c1d43355e5c1787240
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-04-27 13:25:54 -0400

    Safe navigate on top of this?

--- web/src/components/profile/SocialBar.js
+++ web/src/components/profile/SocialBar.js
@@ -7,29 +7,29 @@ import {
 } from "@mui/icons-material";
 
 const SocialBar = ({ links }) => {
-  return links !== null ? (
+  return links ? (
     <Container sx={{ color: "white" }}>
       <Typography variant="h5">Socials: </Typography>
 
-      {links.youtube !== null && (
+      {links?.youtube !== null && (
         <a href={links.youtube}>
           <YouTube fontSize="large" sx={{ color: "#FF0000" }} />
         </a>
       )}
 
-      {links.instagram !== null && (
+      {links?.instagram !== null && (
         <a href={links.instagram}>
           <Instagram fontSize="large" sx={{ color: "#C13584" }} />
         </a>
       )}
 
-      {links.facebook !== null && (
+      {links?.facebook !== null && (
         <a href={links.facebook}>
           <Facebook fontSize="large" sx={{ color: "#4267B2" }} />
         </a>
       )}
 
-      {links.tiktok !== null && (
+      {links?.tiktok !== null && (
         <a href={links.tiktok}>
           <MusicNoteRounded fontSize="large" sx={{ color: "#FE2C55" }} />
         </a>
