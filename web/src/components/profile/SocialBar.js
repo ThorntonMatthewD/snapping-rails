65ba9ea65c11606b669292b0c5bc1786452d9598
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-04-27 12:52:02 -0400

    safely navigate social links

--- web/src/components/profile/SocialBar.js
+++ web/src/components/profile/SocialBar.js
@@ -11,25 +11,25 @@ const SocialBar = ({ links }) => {
     <Container sx={{ color: "white" }}>
       <Typography variant="h5">Socials: </Typography>
 
-      {links.youtube && (
+      {links?.youtube && (
         <a href={links.youtube}>
           <YouTube fontSize="large" sx={{ color: "#FF0000" }} />
         </a>
       )}
 
-      {links.instagram && (
+      {links?.instagram && (
         <a href={links.instagram}>
           <Instagram fontSize="large" sx={{ color: "#C13584" }} />
         </a>
       )}
 
-      {links.facebook && (
+      {links?.facebook && (
         <a href={links.facebook}>
           <Facebook fontSize="large" sx={{ color: "#4267B2" }} />
         </a>
       )}
 
-      {links.tiktok && (
+      {links?.tiktok && (
         <a href={links.tiktok}>
           <MusicNoteRounded fontSize="large" sx={{ color: "#FE2C55" }} />
         </a>
