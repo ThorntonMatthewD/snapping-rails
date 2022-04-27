2f70ed88da8e840c53d14696fd332ef4e4606e99
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-04-27 13:22:38 -0400

    For some reason null isn't being falsey on its own..

--- web/src/components/profile/SocialBar.js
+++ web/src/components/profile/SocialBar.js
@@ -7,29 +7,29 @@ import {
 } from "@mui/icons-material";
 
 const SocialBar = ({ links }) => {
-  return links ? (
+  return links !== null ? (
     <Container sx={{ color: "white" }}>
       <Typography variant="h5">Socials: </Typography>
 
-      {links?.youtube && (
+      {links.youtube !== null && (
         <a href={links.youtube}>
           <YouTube fontSize="large" sx={{ color: "#FF0000" }} />
         </a>
       )}
 
-      {links?.instagram && (
+      {links.instagram !== null && (
         <a href={links.instagram}>
           <Instagram fontSize="large" sx={{ color: "#C13584" }} />
         </a>
       )}
 
-      {links?.facebook && (
+      {links.facebook !== null && (
         <a href={links.facebook}>
           <Facebook fontSize="large" sx={{ color: "#4267B2" }} />
         </a>
       )}
 
-      {links?.tiktok && (
+      {links.tiktok !== null && (
         <a href={links.tiktok}>
           <MusicNoteRounded fontSize="large" sx={{ color: "#FE2C55" }} />
         </a>
